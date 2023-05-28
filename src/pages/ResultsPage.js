a25f3115778760a793a4609346d4a3fab454dbbf
Polcsi <pollakbence12@gmail.com>
2023-05-28 09:37:05 +0200

    responsive company section

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useRef, useCallback } from "react";
 import TopScrollButton from "../components/TopScrollButton";
 import { Link, useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../context";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaHashtag } from "react-icons/fa";
+import { MdOutlineEmail, MdDescription } from "react-icons/md";
 
 const ResultsPage = () => {
   const { dataJSON } = useGlobalContext();
@@ -14,10 +15,32 @@ const ResultsPage = () => {
         {
           name: "Pollák Bence",
           email: "pollakbence12@gmail.com",
-          description: "",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mattis interdum. Integer aliquet nisl ut mi fringilla aliquet. Vestibulum sed scelerisque augue. Sed ut neque nec massa sollicitudin maximus posuere et metus. Vivamus ac ex massa. Phasellus fermentum augue in massa suscipit euismod. Phasellus felis erat, ullamcorper ut sem quis, sollicitudin consequat turpis. Nullam molestie convallis eros, eget dignissim elit blandit eu. Fusce non mi id nunc sollicitudin vehicula. Sed ac sapien vitae sapien aliquet lobortis. In consectetur magna metus, et suscipit metus porttitor quis. Aenean aliquam nunc lectus, ac sollicitudin mauris faucibus eget. Ut imperdiet facilisis turpis, sit amet pellentesque velit iaculis eget. Cras a iaculis lectus, et fringilla mi.",
         },
       ],
       employees: [
+        {
+          name: "asd",
+          age: "32",
+          email: "pollakbence12@gmail.com",
+          job: "software tester",
+          cv: "",
+        },
+        {
+          name: "asd",
+          age: "32",
+          email: "pollakbence12@gmail.com",
+          job: "software tester",
+          cv: "",
+        },
+        {
+          name: "asd",
+          age: "32",
+          email: "pollakbence12@gmail.com",
+          job: "software tester",
+          cv: "",
+        },
         {
           name: "asd",
           age: "32",
@@ -72,10 +95,33 @@ const ResultsPage = () => {
   return (
     <>
       <div className="container results-page">
-        <section className="company-card">
-          <h1>company</h1>
+        <section className="company-card results-section">
+          <h1>company information</h1>
+          <div className="company-details">
+            <div className="c-data-section">
+              <h2>
+                <FaHashtag />
+                name:
+              </h2>
+              <span>{data[0].company[0].name}</span>
+            </div>
+            <div className="c-data-section">
+              <h2>
+                <MdOutlineEmail />
+                email:
+              </h2>
+              <span>{data[0].company[0].email}</span>
+            </div>
+            <div className="c-data-section">
+              <h2>
+                <MdDescription />
+                description:
+              </h2>
+              <span>{data[0].company[0].description}</span>
+            </div>
+          </div>
         </section>
-        <section className="employee-results-section">
+        <section className="employee-results-section results-section">
           <h1>employees</h1>
           <div className="employee-cards on-scroll" ref={employeeCardsRef}>
             {data[0].employees.map((employee, index) => {
